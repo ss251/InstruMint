3509282e2fccd67bb348e3ef0a6bbaf74eb02078
ss251 <ss251@uw.edu>
2022-06-21 18:33:44 -0500

    update campaign contract

--- contracts/CampaignHandler.sol
+++ contracts/CampaignHandler.sol
@@ -19,52 +19,95 @@ contract CampaignHandler {
         uint256 total;
         uint256 startDate;
         address creator;
+        string tokenURI;
     }
+
+    mapping(address => Campaign[]) public creatorToCampaign;
 
     constructor() {
         console.log("Deploying contract CampaignHandler");
     }
 
     function contribute(uint256 amount, uint32 campaignID) external payable {
-        require (msg.value == amount);
+        require(msg.value == amount);
 
         emit LogContribute(msg.sender, amount, campaignID);
 
         require(campaigns[campaignID].isActive == true);
-        require((campaigns[campaignID].total + amount) <= campaigns[campaignID].goal);
+        require(
+            (campaigns[campaignID].total + amount) <= campaigns[campaignID].goal
+        );
 
         campaigns[campaignID].total += amount;
 
-        (uint a, uint b) = checkProgress(campaignID);
+        (uint256 a, uint256 b) = checkProgress(campaignID);
         if (a - b == 0) closeCampaign(campaignID, true);
     }
 
-    function newCampaign(uint256 _goal, string memory _contentName, string memory _contentInfo) external {
+    function newCampaign(
+        uint256 _goal,
+        string memory _contentName,
+        string memory _contentInfo,
+        string memory _tokenURI
+    ) external {
         emit LogNewCampaign(msg.sender, _goal);
         require(_goal > 0, "Campaign goal must be greater than 0 Wei.");
 
-        campaigns.push(Campaign(true, false, _contentName, _contentInfo, _goal, 0, block.timestamp, msg.sender));
+        campaigns.push(
+            Campaign(
+                true,
+                false,
+                _contentName,
+                _contentInfo,
+                _goal,
+                0,
+                block.timestamp,
+                msg.sender,
+                _tokenURI
+            )
+        );
+        creatorToCampaign[msg.sender].push(
+            Campaign(
+                true,
+                false,
+                _contentName,
+                _contentInfo,
+                _goal,
+                0,
+                block.timestamp,
+                msg.sender,
+                _tokenURI
+            )
+        );
     }
 
-    function checkProgress(uint32 campaignID) view public returns (uint256, uint256) {
+    function checkProgress(uint32 campaignID)
+        public
+        view
+        returns (uint256, uint256)
+    {
         return (campaigns[campaignID].goal, campaigns[campaignID].total);
     }
 
     function closeCampaign(uint32 campaignID, bool _isFunded) private {
         campaigns[campaignID].isActive = false;
         campaigns[campaignID].isFunded = _isFunded;
+        creatorToCampaign[campaigns[campaignID].creator][campaignID]
+            .isActive = false;
+        creatorToCampaign[campaigns[campaignID].creator][campaignID]
+            .isFunded = _isFunded;
         emit LogCloseCampaign(campaignID, _isFunded);
     }
 
-    function listActive() view external returns (Campaign[] memory) {
+    function listActive() external view returns (Campaign[] memory) {
         uint256 count = 0;
-        for (uint i = 0; i < campaigns.length; i++) {
+        for (uint256 i = 0; i < campaigns.length; i++) {
             if (campaigns[i].isActive) count++;
         }
         Campaign[] memory _campaigns = new Campaign[](count);
         uint256 j = 0;
-        for (uint i = 0; i < campaigns.length; i++) {
-            if(campaigns[i].isActive) {
+        for (uint256 i = 0; i < campaigns.length; i++) {
+            if (campaigns[i].isActive) {
                 _campaigns[j] = campaigns[i];
                 j++;
             }
@@ -72,7 +115,35 @@ contract CampaignHandler {
         return _campaigns;
     }
 
-    function withdraw(uint256 amount) external {
-
+    function listCreatorCampaigns(address creator)
+        external
+        view
+        returns (Campaign[] memory)
+    {
+        uint256 count = 0;
+        for (uint256 i = 0; i < creatorToCampaign[creator].length; i++) {
+            count++;
+        }
+        Campaign[] memory _campaigns = new Campaign[](count);
+        uint256 j = 0;
+        for (uint256 i = 0; i < creatorToCampaign[creator].length; i++) {
+            _campaigns[j] = creatorToCampaign[creator][i];
+            j++;
+        }
+        return _campaigns;
     }
+
+    function getCampaignCount() public view returns (uint256) {
+        return campaigns.length;
+    }
+
+    function getCampaignTokenURI(uint32 campaignID)
+        external
+        view
+        returns (string memory)
+    {
+        return campaigns[campaignID].tokenURI;
+    }
+
+    function withdraw(uint256 amount) external {}
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,18 +1,39 @@
-//SPDX-License-Identifier: Unlicense
-
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 
 describe("CampaignHandler", function () {
-  it("Should create a new campaign", async function () {
-    const CampaignHandler = await ethers.getContractFactory("CampaignHandler");
-    const campaignHandler = await CampaignHandler.deploy();
-    await campaignHandler.deployed();
+  it("Should create a campaign, contribute to campaign, and close campaign", async function () {
+    const Campaign = await ethers.getContractFactory("CampaignHandler");
+    const campaign = await Campaign.deploy();
+    await campaign.deployed();
+    const campaignAddress = campaign.address;
 
-    const goal = 1000000;
+    await campaign.newCampaign(
+      2,
+      "Drill beat",
+      "Open source beat NFT minted upon campaign sucess.",
+      "https://bafybeif6jx27diov6osmj4nqgyar45qdfestjnrnhomeud26wxa375rxdm.ipfs.dweb.link/4215"
+    );
 
-    expect(await campaignHandler.newCampaign(goal, "Sound Pack 1", "The sound of silence")).to.emit(campaignHandler, "LogNewCampaign");
-    console.log(await campaignHandler.listActive());
+    let campaignId = await campaign.getCampaignCount();
+
+    // checking for campaign creation
+    expect(campaignId.toNumber()).to.equal(1);
+
+    campaignId = campaignId.toNumber() - 1;
+
+    // making 2 contributions
+    await campaign.contribute(1, campaignId, {
+      value: 1,
+    });
+
+    await campaign.contribute(1, campaignId, {
+      value: 1,
+    });
+
+    // checking if the last contribution successfully closed the campaign
+    const activeCampaigns = await campaign.listActive();
+
+    // there should be no active campaigns now
+    expect(activeCampaigns.length).to.equal(0);
   });
-  
 });
